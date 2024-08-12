59e7dd0e8afe0796cf5448cee13960d0114f883a
Aroserot <aroserot@unal.edu.co>
2024-08-12 10:26:13 -0500

    Update Futbolista.java

--- src/main/java/Futbolista.java
+++ src/main/java/Futbolista.java
@@ -1,53 +1,58 @@
 public abstract class Futbolista implements Comparable<Object> {
-    private String nombre;
-    private int edad;
-    private final String posicion;
+	
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	public Futbolista() {
+		this.edad = 30;
+		this.nombre = "Maradona";
+		this.posicion = "delantero";
+	}
 
-    public Futbolista(String nombre, int edad, String posicion) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.posicion = posicion;
-    }
+	public Futbolista(String nombre, int edad, String posicion) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+	
+	@Override
+	public String toString() {
+		return "El futbolista "+getNombre()+" tiene "+getEdad()+ ", y juega de "+getPosicion();
+	}
+	
+	public boolean equals(Futbolista f) {
+		
+		if (f == null) {
+			return false;
+		}
 
-    public Futbolista() {
-        this("Maradona", 30, "delantero");
-    }
+		return this == f;
+	}
+	
+	//Metodo abstracto
+	public abstract boolean jugarConLasManos();
+	
 
-    @Override
-    public String toString() {
-        return "El futbolista " + getNombre() + " tiene " + getEdad() + ", y juega de " + getPosicion();
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
+	public String getNombre() {
+		return nombre;
+	}
 
-        if (obj == null || getClass() != obj.getClass()) return false; 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-        Futbolista f = (Futbolista) obj;
-        return edad == f.edad && nombre.equals(f.nombre) && posicion.equals(f.posicion);
-    }
+	public int getEdad() {
+		return edad;
+	}
 
-    public abstract boolean jugarConLasManos(); 
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getPosicion() {
-        return posicion;
-    }
+	public String getPosicion() {
+		return posicion;
+	}
 
 }
