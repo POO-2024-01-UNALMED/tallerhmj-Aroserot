68b6d08a3fe8d544fc21785f64ec452da26f7edb
Aroserot <aroserot@unal.edu.co>
2024-08-12 10:24:25 -0500

    cambio 2

---
+++ src/main/java/Portero.java
@@ -0,0 +1,35 @@
+public class Portero extends Futbolista {
+	
+	public short golesRecibidos;
+	public byte dorsal;
+
+	public Portero() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Portero(String nombre, int edad, short golesR, byte dorsal) {
+		super(nombre, edad, "Portero");
+		golesRecibidos = golesR;
+		this.dorsal = dorsal;
+		
+	}
+
+	@Override
+	public int compareTo(Object object) {
+		Portero portero = (Portero) object;
+		
+		return Math.abs(this.golesRecibidos - portero.golesRecibidos);
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "El futbolista " + getNombre() + " tiene " + getEdad() + ", y juega de " + getPosicion() + " con el dorsal " + dorsal + ". Le han marcado " + golesRecibidos;
+	}
+	
+}
