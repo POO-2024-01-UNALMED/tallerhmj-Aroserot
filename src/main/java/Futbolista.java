692907fe436dd55a76c6721d143bd21dd0ac0f4f
Aroserot <aroserot@unal.edu.co>
2024-08-12 10:19:51 -0500

    Deportes

---
+++ src/main/java/Futbolista.java
@@ -0,0 +1,53 @@
+public abstract class Futbolista implements Comparable<Object> {
+    private String nombre;
+    private int edad;
+    private final String posicion;
+
+    public Futbolista(String nombre, int edad, String posicion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.posicion = posicion;
+    }
+
+    public Futbolista() {
+        this("Maradona", 30, "delantero");
+    }
+
+    @Override
+    public String toString() {
+        return "El futbolista " + getNombre() + " tiene " + getEdad() + ", y juega de " + getPosicion();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false; 
+
+        Futbolista f = (Futbolista) obj;
+        return edad == f.edad && nombre.equals(f.nombre) && posicion.equals(f.posicion);
+    }
+
+    public abstract boolean jugarConLasManos(); 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+}
